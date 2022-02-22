8385ad9e1966ef7ff2e18f5f33734fa3d2ce780e
Pamba Shatson Fasco <pambakev1@gmail.com>
2022-02-22 16:33:16 +0300

    flutter initial commit

---
+++ lib/api_manager.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:soccer_tutorial/soccermodel.dart';
+
+class SoccerApi {
+  final String apiUrl =
+      "https://v3.football.api-sports.io/fixtures?season=2020&league=39";
+
+  static const headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': "PUT YOUR API KEY HERE OR IT WILL NOT WORK"
+  };
+
+  Future<List<SoccerMatch>> getAllMatches() async {
+    Response res = await get(apiUrl, headers: headers);
+    var body;
+
+    if (res.statusCode == 200) {
+      body = jsonDecode(res.body);
+      List<dynamic> matchesList = body['responses'];
+      print("Api service: ${body}");
+      List<SoccerMatch> matches = matchesList
+          .map((dynamic item) => SoccerMatch.fromJson(item))
+          .toList();
+
+      return matches;
+    }
+  }
+}
